80b39ddff0fe4956b182f36fea1b8c94038aa550
piktoraulia5-beep <piktoraulia5@gmail.com>
2026-06-17 12:13:57 +0700

    Add files via upload

---
+++ javacrud-piktoraulia/README.md
@@ -0,0 +1,12 @@
+xampp
+
+
+CREATE DATABASE tutorialjava;
+
+CREATE TABLE students(
+ nis int PRIMARY KEY,
+ nama varchar(100),
+ jurusan varchar(100),
+ jk varchar(16),
+ alamat text
+);
---
+++ javacrud-piktoraulia/src/main/java/test/javacrud/Javacrud.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test.javacrud;
+
+/**
+ *
+ * @author rvngm
+ */
+public class Javacrud {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
